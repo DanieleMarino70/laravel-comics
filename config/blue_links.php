352788403d2d4055bdf6b9ed7c65338f62de8991
DanieleMarino70 <danielaotitan@gmail.com>
2023-03-28 21:44:22 +0200

    add: bluemenu

---
+++ config/blue_links.php
@@ -0,0 +1,32 @@
+<?php
+
+return
+    [
+        [
+            "title" => "Digital Comics",
+            "url" => "#",
+            "src" =>  "/buy-comics-digital-comics.png"
+        ],
+        [
+            "title" => "Dc Merchandise",
+            "url" => "#",
+            "src" =>  "/buy-comics-merchandise.png"
+        ],
+        [
+            "title" => "Subscription",
+            "url" => "#",
+            "src" =>  "/buy-comics-subscriptions.png"
+        ],
+        [
+            "title" => "Comic Shop Locator",
+            "url" => "#",
+            "src" =>  "/buy-comics-shop-locator.png"
+        ],
+        [
+            "title" => "Dc Power Visa",
+            "url" => "#",
+            "src" =>  "/buy-dc-power-visa.svg"
+        ]
+
+
+    ];
--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,9 +4,7 @@
 
 <div class="comics-content">
     <div class="content">
-        <div class="jumbotron">
-            
-        </div>
+        <div class="jumbotron"></div>
             <div class="container">
                 <div class="row row-cols-lg-6 row-cols-md-4">
                     @foreach ($comics as $comic)
@@ -22,7 +20,28 @@
                     </div>
                     @endforeach
                 </div>
+                <div class="d-flex justify-content-center">
+
+                    <button type="button" class="btn btn-primary rounded-0">LOAD MORE</button>
+                </div>
             </div>
+
+        </div>
+
+        <div class="background">
+                <div class="container">
+                    <ul>
+                        @foreach ($blue_links as $link)
+                        <li>
+                            <div class="img-container">
+                                <img src='{{ asset("images{$link['src']}") }}' alt="">
+                            </div>
+                            <a href={{$link['url']}}>{{ $link['title'] }}</a>
+                        </li>    
+                        @endforeach
+                        
+                    </ul>
+                </div>
         </div>
 </div>
 @endsection
